287dbf1ea2103ad0859dd8729a9d986f11b072cf
Dhruv Kurliye <dhruvk@cdac.in>
2026-05-26 13:13:27 +0530

    replit commit

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
+import os
+
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=3000, reload=True)
+    port = int(os.environ.get("PORT", 3000))
+    reload = os.environ.get("REPL_ID") is None
+
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=port,
+        reload=reload
+    )
